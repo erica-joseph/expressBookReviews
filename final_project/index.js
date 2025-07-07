dd789fb76b7621cbe463826609f5d029d43849f3
Erica Joseph <84351866+erica-joseph@users.noreply.github.com>
2025-07-07 10:21:25 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session');
-const { authenticated } = require('./router/auth_users.js');
+const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-const books_route = require('./router/booksdb.js');
-
 
 const app = express();
 
@@ -13,39 +10,9 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/books", books_route);
-
-
-app.use("/customer/auth/*", function auth(req, res, next) {
-    // Check if the user is authenticated via session
-
-        // Check if there's a JWT token in the request headers
-console.log(req.session)
-
-        if(req.session){
-            if(req.session.authorization){
-                    let token = req.session.authorization.token;
-                    jwt.verify(token, 'my-secret-key', (err, user) => {
-                        if (err) {
-                            // Token verification failed
-                            res.status(401).json({ error: 'galek err' });
-                        } else {
-                            req.session.user = user;
-                            next();
-                        }
-                    });
-
-
-            }
-            else{
-                res.status(401).json({ error: 'Unauthorized here' });
-            }
-        }
-
-
-    
+app.use("/customer/auth/*", function auth(req,res,next){
+//Write the authenication mechanism here
 });
-
  
 const PORT =5000;
 
